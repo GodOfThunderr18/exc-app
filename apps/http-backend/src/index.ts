23cd446c2bef1adad819657a745a5c2347ca6d0e
Sushanth <sushanthkartikeya18@gmail.com>
2026-01-15 12:55:51 +0530

    su si room endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'HTTP Backend is running!' });
+app.post("/signin", (req, res) => {
+  
+    const userId=1;
+    const token=jwt.sign({userId},JWT_SECRET);
+
+    res.json({token});
+});
+
+app.post("/signup", (req, res) => {
+     res.json({userId:"123"})
+});
+
+app.post("/room",middleware,(req, res) => {
+     res.json({
+        roomId: "room-123",
+     })
 });
 
 app.listen(PORT, () => {
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,24 @@
+
+import {Request,Response,NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+export function middleware(req:Request,res:Response,next:NextFunction){
+    const token=req.headers['authorization'] ?? "";
+
+    const decoded=jwt.verify(token,JWT_SECRET);
+
+    if(decoded && typeof decoded === 'object' && 'userId' in decoded){
+       req.userId=decoded.userId as string;
+       next();
+    }else{
+        res.status(401).json({message:"Unauthorized"}); 
+    }
+}
